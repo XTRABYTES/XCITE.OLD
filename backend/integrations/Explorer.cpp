7d81aba9d39be6aa435b6a53f61cff82c36bf9fe
jpilgrim6 <jpilgrim6@gmail.com>
2019-04-19 10:42:04 -0400

    adding default locale

--- backend/integrations/Explorer.cpp
+++ backend/integrations/Explorer.cpp
@@ -85,6 +85,7 @@ void Explorer::getDetails(QString coin, QString transaction) {
             QJsonObject result = jsonResponse.object().value("result").toObject();
 
             QString timestamp = result.value("data").toString();
+            QLocale::setDefault(QLocale::English);
             QDateTime date = QLocale().toDateTime(timestamp, "yyyy-MMM-dd HH:mm:ss");
             date.setTimeSpec(Qt::UTC);
             QDateTime local = date.toLocalTime();
